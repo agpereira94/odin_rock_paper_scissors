ca1dd762777352ba0d849f1557d3768794a9ba33
agpereira94 <pereirinha94@gmail.com>
2025-05-12 07:47:41 +0200

    Revised basic game and round play
    - Round playing is now working as expected

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -14,35 +14,54 @@ function getComputerChoice(){
     return computerChoice;
 }
 
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
 
 function getHumanChoice(){
-    const humanChoice = prompt("It's your play: rock, paper or scissors?");
+    const humanChoice = prompt("What's it gonna be: rock, paper or scissors?");
 
     return humanChoice;
 }
 
-console.log(getHumanChoice());
+// console.log(getHumanChoice());
 
 let playerScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice){
 
-        if(finalHumanChoice.toUpperCase() === computerChoice.toUpperCase()){
-            return "It's a draw.";
-        } else if(finalHumanChoice.toUpperCase() === "ROCK" && computerChoice === "scissors"){
-            return "You win! Rock beats scissors";
-        } else if(finalHumanChoice.toUpperCase() === "ROCK" && computerChoice === "paper"){
-            return "You lose! Paper beats rock.";
-        } else if(finalHumanChoice.toUpperCase() === "PAPER" && computerChoice === "rock"){
-            return "You win! Paper beats rock";
-        } else if(finalHumanChoice.toUpperCase() === "PAPER" && computerChoice === "scissors"){
-            return "You lose! Scissors beats paper";
-        } else if(finalHumanChoice.toUpperCase() === "SCISSORS" && computerChoice === " rock"){
-            return "You lose! Rock beats scissors";
-        } else if(finalHumanChoice.toUpperCase() === "SCISSORS" && computerChoice === "paper"){
-            return "You win! Scissors beats paper";
+        if(humanChoice.toUpperCase() === "ROCK" && computerChoice.toUpperCase() === "ROCK"){
+            return alert("It's a draw. Can you hear the stone hinge?");
+        } else if(humanChoice.toUpperCase() === "PAPER" && computerChoice.toUpperCase() === "PAPER") {
+            return alert("It's a draw. Can you hear the scratching?");
+        } else if(humanChoice.toUpperCase() === "SCISSORS" && computerChoice.toUpperCase() === "SCISSORS") {
+            return alert("It's a draw. Can you hear the clinking?");
+        } else if(humanChoice.toUpperCase() === "ROCK" && computerChoice === "scissors"){
+            playerScore +=1;
+            return alert("You win! Rock beats scissors."); 
+        } else if(humanChoice.toUpperCase() === "ROCK" && computerChoice === "paper"){
+            computerScore +=1;
+            return alert("You lose! Paper beats rock.");
+        } else if(humanChoice.toUpperCase() === "PAPER" && computerChoice === "rock"){
+            playerScore +=1;
+            return alert("You win! Paper beats rock.");
+        } else if(humanChoice.toUpperCase() === "PAPER" && computerChoice === "scissors"){
+            computerScore +=1;
+            return alert("You lose! Scissors beats paper.");
+        } else if(humanChoice.toUpperCase() === "SCISSORS" && computerChoice === " rock"){
+            computerScore +=1;
+            return alert("You lose! Rock beats scissors.");
+        } else if(humanChoice.toUpperCase() === "SCISSORS" && computerChoice === "paper"){
+            playerScore +=1;
+            return alert("You win! Scissors beats paper.");
         }
 
-}
+        
+    }
+    
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    
+    playRound(humanSelection, computerSelection);
+    
+    console.log(computerScore);
+    console.log(playerScore);
