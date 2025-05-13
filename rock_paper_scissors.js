490472fe4f81c18cfd18243571e43308a15cea0b
agpereira94 <pereirinha94@gmail.com>
2025-05-13 20:07:33 +0200

    Finished coding the game

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -28,40 +28,85 @@ let playerScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice){
-
-        if(humanChoice.toUpperCase() === "ROCK" && computerChoice.toUpperCase() === "ROCK"){
-            return alert("It's a draw. Can you hear the stone hinge?");
-        } else if(humanChoice.toUpperCase() === "PAPER" && computerChoice.toUpperCase() === "PAPER") {
-            return alert("It's a draw. Can you hear the scratching?");
-        } else if(humanChoice.toUpperCase() === "SCISSORS" && computerChoice.toUpperCase() === "SCISSORS") {
-            return alert("It's a draw. Can you hear the clinking?");
-        } else if(humanChoice.toUpperCase() === "ROCK" && computerChoice === "scissors"){
-            playerScore +=1;
-            return alert("You win! Rock beats scissors."); 
-        } else if(humanChoice.toUpperCase() === "ROCK" && computerChoice === "paper"){
-            computerScore +=1;
-            return alert("You lose! Paper beats rock.");
-        } else if(humanChoice.toUpperCase() === "PAPER" && computerChoice === "rock"){
-            playerScore +=1;
-            return alert("You win! Paper beats rock.");
-        } else if(humanChoice.toUpperCase() === "PAPER" && computerChoice === "scissors"){
-            computerScore +=1;
-            return alert("You lose! Scissors beats paper.");
-        } else if(humanChoice.toUpperCase() === "SCISSORS" && computerChoice === " rock"){
-            computerScore +=1;
-            return alert("You lose! Rock beats scissors.");
-        } else if(humanChoice.toUpperCase() === "SCISSORS" && computerChoice === "paper"){
-            playerScore +=1;
-            return alert("You win! Scissors beats paper.");
-        }
-
         
-    }
+    if(humanChoice.toUpperCase() === "ROCK" && computerChoice.toUpperCase() === "ROCK"){
+        alert("It's a draw. Can you hear the stone hinge?");
+        return "draw";
+    } else if(humanChoice.toUpperCase() === "PAPER" && computerChoice.toUpperCase() === "PAPER") {
+        alert("It's a draw. Can you hear the scratching?")
+        return "draw";
+    } else if(humanChoice.toUpperCase() === "SCISSORS" && computerChoice.toUpperCase() === "SCISSORS") {
+        alert("It's a draw. Can you hear the clinking?")
+        return "draw";
+    } else if(humanChoice.toUpperCase() === "ROCK" && computerChoice === "scissors"){
+        playerScore +=1;
+        alert("You win! Rock beats scissors.")
+        return "win"; 
+    } else if(humanChoice.toUpperCase() === "ROCK" && computerChoice === "paper"){
+        computerScore +=1;
+        alert("You lose! Paper beats rock.")
+        return "lose";
+    } else if(humanChoice.toUpperCase() === "PAPER" && computerChoice === "rock"){
+        playerScore +=1;
+        alert("You win! Paper beats rock.")
+        return "win";
+    } else if(humanChoice.toUpperCase() === "PAPER" && computerChoice === "scissors"){
+        computerScore +=1;
+        alert("You lose! Scissors beats paper.")
+        return "lose";
+    } else if(humanChoice.toUpperCase() === "SCISSORS" && computerChoice === " rock"){
+        computerScore +=1;
+        alert("You lose! Rock beats scissors.")
+        return "lose";
+    } else if(humanChoice.toUpperCase() === "SCISSORS" && computerChoice === "paper"){
+        playerScore +=1;
+        alert("You win! Scissors beats paper.")
+        return "win";
+    }   
+}
+
+
+
+// playRound(humanSelection, computerSelection);
+
+
+function playGame(){
     
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+
     playRound(humanSelection, computerSelection);
-    
-    console.log(computerScore);
-    console.log(playerScore);
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+    return;
+
+}
+
+playGame();
+
+if (playerScore > computerScore){
+    alert(`CONGRATS, CHAMP! You won.
+        Computer score: ${computerScore}
+        Your score: ${playerScore}`);
+} else if (playerScore === computerScore){
+    alert(`It's a tie. What are the odds?
+        Computer score: ${computerScore}
+        Your score: ${playerScore}`);
+} else {
+    alert(`LOSER. This one goes to the computer.
+        Computer score: ${computerScore}
+        Your score: ${playerScore}`);
+}
+
+// console.log(computerScore);
+// console.log(playerScore);
